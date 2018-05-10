66b94fb2b836bcaae39bc00b01cf2c2fb4e05e1e
Byron Murray <byron4stacey@hotmail.com>
2018-05-10 16:37:21 +1200

    tabs

---
+++ resources/assets/js/components/tab.vue
@@ -0,0 +1,28 @@
+<template>
+
+      <div class="">
+        <slot></slot>
+      </div>
+
+
+</template>
+
+<script>
+    export default {
+
+        props: {
+          name: { required: true },
+          selected: { default: false }
+        },
+        data() {
+          return {
+            isActive: false
+          };
+        },
+        mounted() {
+          this.isActive = this.selected;
+          console.log('Component mounted.')
+        }
+    }
+
+</script>
---
+++ resources/assets/js/components/tabs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="">
+
+    <div class="tabs">
+      <ul>
+
+        <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+          <a href="#" @click="selectTab(tab)">{{ tab.name }}</a>
+        </li>
+
+      </ul>
+    </div>
+
+    <div class="tabs-details">
+      <slot></slot>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+    import tab from './tab.vue'
+
+    export default {
+        data() {
+          return { tabs: [] };
+        },
+        created() {
+          this.tabs = this.$children;
+        },
+        methods: {
+          selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+              tab.isActive = (tab.name == selectedTab.name);
+            });
+          }
+        },
+        mounted() {
+            console.log(this.$children)
+        }
+    }
+
+</script>
